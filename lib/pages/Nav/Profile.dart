8469a2e2091575a67b4811edb80f9e9fa471f7c3
tsumith <tsumith987@gmail.com>
2024-11-08 01:04:36 +0530

    button update

--- lib/pages/Nav/Profile.dart
+++ lib/pages/Nav/Profile.dart
@@ -8,14 +8,52 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  Color myCol = const Color.fromARGB(255, 20, 54, 116);
+  Color _changeColor(Color col) {
+    var myCol = col;
+
+    if (myCol == Color.fromARGB(255, 20, 54, 116)) {
+      myCol = Colors.blue;
+    } else {
+      myCol = Color.fromARGB(255, 20, 54, 116);
+    }
+    return myCol;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Stack(
       children: [
         BlackGreyGradBackground(),
-        Center(
-          child: WhiteText(data: "user profile and data"),
+        ListView(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                var col = _changeColor(myCol);
+                myCol = col;
+                setState(() {});
+              },
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 15),
+                child: Center(
+                  child: WhiteText(data: "settings"),
+                ),
+                height: 60,
+                width: 200,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      myCol,
+                      const Color.fromARGB(255, 27, 62, 109),
+                      myCol
+                    ]),
+                    borderRadius: BorderRadius.circular(30)),
+              ),
+            )
+          ],
         )
       ],
     ));
